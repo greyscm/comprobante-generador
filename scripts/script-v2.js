dddabf175523b7800d00f501e71b45ce68aa37da
greycm mac <cm.greys@gmail.com>
2024-11-06 13:09:34 -0300

    .

--- scripts/script-v2.js
+++ scripts/script-v2.js
@@ -17,11 +17,13 @@ function generarComprobante() {
     // Formatear ID de transacción
     const año = String(fechaObj.getFullYear()).slice(2);
     const mes = String(fechaObj.getMonth() + 1).padStart(2, '0');
-    const día = String(fechaObj.getDate()).padStart(2, '0');
+    const dia = String(fechaObj.getDate()).padStart(2, '0');
     const hora = String(fechaObj.getHours()).padStart(2, '0');
     const minutos = String(fechaObj.getMinutes()).padStart(2, '0');
+    
     const fechaParaId = año + mes + dia + hora + minutos;
 
+    // Generar 8 números aleatorios
     const generarSecuenciaNumerica = (longitud) => {
         let secuencia = '';
         for (let i = 0; i < longitud; i++) {
@@ -30,6 +32,7 @@ function generarComprobante() {
         return secuencia;
     };
 
+    // Agregar "11685" seguido de 8 números aleatorios
     const secuenciaNumerica = '11685' + generarSecuenciaNumerica(8);
     const transaccionId = 'TEFMBCO' + fechaParaId + secuenciaNumerica;
 
@@ -37,18 +40,4 @@ function generarComprobante() {
 
     const url = `comprobante.html?nombre=${encodeURIComponent(nombre)}&bancoDestino=${encodeURIComponent(bancoDestino)}&tipoCuenta=${encodeURIComponent(tipoCuenta)}&numeroCuenta=${encodeURIComponent(maskedNumeroCuenta)}&monto=${encodeURIComponent(formattedMonto)}&fecha=${encodeURIComponent(fecha)}&transaccionId=${encodeURIComponent(transaccionId)}`;
     window.location.href = url;
-}
-
-function copiarDatos() {
-    const nombre = document.getElementById('nombre').value;
-    const bancoDestino = document.getElementById('bancoDestino').value;
-    const tipoCuenta = document.getElementById('tipoCuenta').value;
-    const numeroCuenta = document.getElementById('numeroCuenta').value;
-
-    const datos = `Nombre: ${nombre}\nBanco destino: ${bancoDestino}\nTipo de cuenta: ${tipoCuenta}\nN° de cuenta: ${numeroCuenta}`;
-    navigator.clipboard.writeText(datos).then(() => {
-        alert('Datos copiados al portapapeles');
-    }, (err) => {
-        console.error('Error al copiar al portapapeles: ', err);
-    });
 }
