301f6a71df86dfe92bd9a80ef1ec929e38c0c469
greycm mac <cm.greys@gmail.com>
2024-11-07 10:25:43 -0300

    back

--- scripts/script.js
+++ scripts/script.js
@@ -7,19 +7,22 @@ function generarComprobante() {
 
     const maskedNumeroCuenta = tipoCuenta + ' ****' + numeroCuenta.slice(-4);
 
-    // Generar fecha y hora sin coma
+    // Generar fecha y hora en el formato deseado
     const fechaObj = new Date();
-    const opcionesFecha = { year: 'numeric', month: 'long', day: 'numeric' };
-    const fechaFormato = fechaObj.toLocaleDateString('es-ES', opcionesFecha);
-    const horaFormato = fechaObj.toLocaleTimeString('es-ES');
-    const fecha = `${fechaFormato} ${horaFormato}`;
+    const dia = String(fechaObj.getDate()).padStart(2, '0'); // Día con dos dígitos
+    const mes = fechaObj.toLocaleString('es-ES', { month: 'long' }); // Mes en texto
+    const año = fechaObj.getFullYear();
+    const hora = String(fechaObj.getHours()).padStart(2, '0'); // Formato 24 horas
+    const minutos = String(fechaObj.getMinutes()).padStart(2, '0');
+
+    const fecha = `${dia} de ${mes} del ${año} ${hora}:${minutos} hrs.`;
 
     // Formatear ID de transacción
-    const año = String(fechaObj.getFullYear()).slice(-2);
-    const mes = String(fechaObj.getMonth() + 1).padStart(2, '0');
-    const día = String(fechaObj.getDate()).padStart(2, '0');
-    const hora = String(fechaObj.getHours()).padStart(2, '0');
-    const minutos = String(fechaObj.getMinutes()).padStart(2, '0');
+    const añoTransaccion = String(fechaObj.getFullYear()).slice(-2);
+    const mesTransaccion = String(fechaObj.getMonth() + 1).padStart(2, '0');
+    const diaTransaccion = String(fechaObj.getDate()).padStart(2, '0');
+    const horaTransaccion = String(fechaObj.getHours()).padStart(2, '0');
+    const minutosTransaccion = String(fechaObj.getMinutes()).padStart(2, '0');
 
     // Generar 7 números aleatorios para el ID
     const generarSecuenciaNumerica = (longitud) => {
@@ -33,7 +36,7 @@ function generarComprobante() {
     const secuenciaAleatoria = generarSecuenciaNumerica(7);
 
     // Componer el transaccionId en el formato deseado
-    const transaccionId = `TEFMBCO${año}${mes}${día}${hora}${minutos}11685${secuenciaAleatoria}`;
+    const transaccionId = `TEFMBCO${añoTransaccion}${mesTransaccion}${diaTransaccion}${horaTransaccion}${minutosTransaccion}11685${secuenciaAleatoria}`;
 
     const formattedMonto = new Intl.NumberFormat('es-ES', { style: 'currency', currency: 'CLP' }).format(monto).replace('CLP', '').trim();
 
